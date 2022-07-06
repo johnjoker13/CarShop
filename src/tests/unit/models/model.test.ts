bedf65aa7560956c810f40efef8396b59c0895d6
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-06 18:08:30 -0300

    test(model.test): creates model test structure

---
+++ src/tests/unit/models/model.test.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import mongoose from 'mongoose';
+import Sinon from 'sinon';
+import sinon, { SinonStub } from 'sinon';
+
+describe('Model', () => {
+  describe('Create car', () => {
+    before(() => {
+      sinon.stub().resolves();
+    });
+
+    after(() => {
+    });
+
+    it('Success', async () => {
+      
+    });
+  });
+});
