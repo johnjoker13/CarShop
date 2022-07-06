bf01e31741c9488f485779e1cde444e78080214e
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-06 17:14:39 -0300

    feat(CarInterface.ts): implements Car interface

---
+++ src/interfaces/CarInterface.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { VehicleSchema } from './VehicleInterface';
+
+const CarSchema = VehicleSchema.extend({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
+});
+
+type Car = z.infer<typeof CarSchema>;
+
+export { CarSchema, Car };
