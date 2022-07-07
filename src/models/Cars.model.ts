9648a55def42f12d84c7b8732ed61615956731ca
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-07 18:27:15 -0300

    feat(Cars.model.ts): implements model for Cars

---
+++ src/models/Cars.model.ts
@@ -0,0 +1,22 @@
+import { model as createModel, Schema } from 'mongoose';
+import { Car } from '../interfaces/CarInterface';
+import MongoModel from './MongoModel';
+import CarDocument from '../interfaces/CardDocument';
+
+const carSchema = new Schema<CarDocument>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class CarsModel extends MongoModel<Car> {
+  constructor(model = createModel('Cars', carSchema)) {
+    super(model);
+  }
+}
+
+export default CarsModel;
