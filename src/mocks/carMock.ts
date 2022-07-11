21562f6b327a2cf03dfead2dd109367bae7a3310
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-11 18:38:07 -0300

    test(carMock): creates mock for Car type

---
+++ src/mocks/carMock.ts
@@ -0,0 +1,13 @@
+import { Car } from '../interfaces/CarInterface';
+
+const carMock: Car = {
+  model: 'Tesla Model X',
+  year: 2022,
+  color: 'Blue',
+  status: true,
+  buyValue: 500000,
+  doorsQty: 4,
+  seatsQty: 5,  
+};
+
+export default carMock;
