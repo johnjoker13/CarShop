2ed90d44f553b329dd8f25db1f7018b29cb4447f
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-11 18:39:26 -0300

    test(model.test): implements CarModel tests

--- src/tests/unit/models/model.test.ts
+++ src/tests/unit/models/model.test.ts
@@ -1,19 +1,32 @@
 import { expect } from 'chai';
-import mongoose from 'mongoose';
-import Sinon from 'sinon';
+import mongoose, { model } from 'mongoose';
 import sinon, { SinonStub } from 'sinon';
+import { Car } from '../../../interfaces/CarInterface';
+import CarsModel, { carSchema } from '../../../models/Cars.model';
+import { carMock } from '../../../mocks';
 
 describe('Model', () => {
+  const carMongooseModel = model<Car>('Cars', carSchema);
+
   describe('Create car', () => {
     before(() => {
-      sinon.stub().resolves();
+      sinon.stub(carMongooseModel, 'create').resolves(carMock);
     });
 
     after(() => {
+      (carMongooseModel.create as SinonStub).restore();
     });
 
-    it('Success', async () => {
-      
+    it('1 - New car successfully created returns a new car obj', async () => {
+      const carModel = new CarsModel();
+      const carCreated = await carModel.create(carMock);
+      expect(carCreated).to.be.deep.equal(carMock);
+    });
+
+    it('2 - New car has a property called "model"', async () => {
+      const carModel = new CarsModel();
+      const carCreated = await carModel.create(carMock);
+      expect(carCreated).to.have.property('model');
     });
   });
 });
