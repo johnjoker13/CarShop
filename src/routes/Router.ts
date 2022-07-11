b4528852b79e65da35e0116d2abfceafc8d9e70d
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-11 17:35:28 -0300

    feat(Router.ts): implements CustomRouter class

---
+++ src/routes/Router.ts
@@ -0,0 +1,23 @@
+// src/routes/Router.ts
+
+import { Router } from 'express';
+import MongoController from '../controllers/MongoController';
+
+class CustomRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(
+    controller: MongoController<T>,
+    route: string = controller.route,
+  ) {
+    // this.router.get(route, controller.read);
+    // this.router.get(`${route}/:id`, controller.readOne);
+    this.router.post(route, controller.create);
+  }
+}
+
+export default CustomRouter;
