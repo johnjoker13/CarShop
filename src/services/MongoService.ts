2f64be4177de5d7548001132af6bdeb4a682deb3
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-11 17:31:03 -0300

    feat(MongoService): implements abstract class for services

--- src/services/MongoService.ts
+++ src/services/MongoService.ts
@@ -1,11 +1,27 @@
+import { ZodError } from 'zod';
 import MongoModel from '../models/MongoModel';
+
+export interface ServiceError {
+  error: ZodError;
+}
 
 abstract class MongoService<T> {
   constructor(protected model: MongoModel<T>) { }
 
-  public create = async (obj: T): Promise<T | null> => this.model.create(obj);
+  public create = async (obj: T): Promise<T | null | ServiceError> => 
+    this.model.create(obj);
 
   public read = async (): Promise<T[]> => this.model.read();
+
+  public readOne = async (id: string): Promise<T | null | ServiceError> =>
+    this.model.readOne(id);
+
+  public update =
+  async (id: string, obj: T): Promise<T | null | ServiceError> =>
+    this.model.update(id, obj);
+  
+  public delete = async (id: string): Promise<T | null | ServiceError> =>
+    this.model.delete(id);
 }
 
 export default MongoService;
