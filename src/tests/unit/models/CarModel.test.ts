70e7dc8a4a0779c9d2643aea37acf039321fca5f
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-12 10:50:58 -0300

    test(CarModel): implements test for Car model

---
+++ src/tests/unit/models/CarModel.test.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import { model } from 'mongoose';
+import sinon, { SinonStub } from 'sinon';
+import { Car } from '../../../interfaces/CarInterface';
+import CarsModel, { carSchema } from '../../../models/Cars.model';
+import { carMock } from '../../../mocks';
+
+describe('Car Model', () => {
+  const carMongooseModel = model<Car>('Cars', carSchema);
+
+  describe('Create car', () => {
+    before(() => {
+      sinon.stub(carMongooseModel, 'create').resolves(carMock);
+    });
+
+    after(() => {
+      (carMongooseModel.create as SinonStub).restore();
+    });
+
+    it('1 - New car successfully created returns a new car obj', async () => {
+      const carModel = new CarsModel();
+      const carCreated = await carModel.create(carMock);
+      expect(carCreated).to.be.deep.equal(carMock);
+    });
+
+    it('2 - New car has a property called "model"', async () => {
+      const carModel = new CarsModel();
+      const carCreated = await carModel.create(carMock);
+      expect(carCreated).to.have.property('model');
+    });
+  });
+});
