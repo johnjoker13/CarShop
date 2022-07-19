6f5e7173ea24ae7a2c44b179edd4743a16136afe
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-19 17:04:36 -0300

    feat(Motorcycle.model): creates model for motorcycles

---
+++ src/models/Motorcycle.model.ts
@@ -0,0 +1,25 @@
+import { model as createModel, Schema } from 'mongoose';
+import { Motorcycle } from '../interfaces/MotorcycleInterface';
+import MongoModel from './MongoModel';
+import MotorcycleDocument from '../interfaces/MotorcycleDocument';
+
+export const motorcycleSchema = new Schema<MotorcycleDocument>({
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  color: { type: String, required: true },
+  status: Boolean,
+  buyValue: { type: Number, required: true },
+  category: { type: String, required: true },
+  engineCapacity: { type: Number, required: true },
+}, { versionKey: false });
+
+export const motorcycleModel = createModel<MotorcycleDocument
+>('Motorcycle', motorcycleSchema);
+
+class MotorcycleModel extends MongoModel<Motorcycle> {
+  constructor(model = motorcycleModel) {
+    super(model);
+  }
+}
+
+export default MotorcycleModel;
