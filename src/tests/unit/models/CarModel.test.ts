06913c1b214e28c660aa0b0dab7b8899c4a70070
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-19 15:56:50 -0300

    test(CarModel): implementes test for update and delete

--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -3,7 +3,8 @@ import { model } from 'mongoose';
 import sinon, { SinonStub } from 'sinon';
 import { Car } from '../../../interfaces/CarInterface';
 import CarsModel, { carModel as mongoModel } from '../../../models/Cars.model';
-import { carMock } from '../../../mocks';
+import { carMock, createdCarMock, readCarsOne } from '../../../mocks';
+import CarDocument from '../../../interfaces/CardDocument';
 
 const carModel = new CarsModel();
 
@@ -18,13 +19,11 @@ describe('Car Model', () => {
     });
 
     it('1 - New car successfully created returns a new car obj', async () => {
-      const carModel = new CarsModel();
       const carCreated = await carModel.create(carMock);
       expect(carCreated).to.be.deep.equal(carMock);
     });
 
     it('2 - New car has a property called "model"', async () => {
-      const carModel = new CarsModel();
       const carCreated = await carModel.create(carMock);
       expect(carCreated).to.have.property('model');
     });
@@ -40,9 +39,54 @@ describe('Car Model', () => {
     });
 
     it('1 - Returns an array of cars', async () => {
-      const carModel = new CarsModel();
       const carCreated = await carModel.read();
       expect(carCreated).to.be.deep.equal([]);
+    });
+  });
+
+  describe('ReadOne car', () => {
+    before(() => {
+      sinon.stub(mongoModel, 'findOne').resolves(readCarsOne as (CarDocument & { _id: any }));
+    });
+
+    after(() => {
+      (mongoModel.findOne as SinonStub).restore();
+    });
+
+    it('1 - Returns matched car', async () => {
+      const carCreated = await carModel.readOne('4edd40c86762e0fb12000003');
+      expect(carCreated).to.be.equal(readCarsOne);
+    });
+  });
+
+  describe('update car', () => {
+    before(() => {
+      sinon.stub(mongoModel, 'findOneAndUpdate').resolves(createdCarMock as (CarDocument & { _id: any }));
+    });
+
+    after(() => {
+      (mongoModel.findOneAndUpdate as SinonStub).restore();
+    });
+
+    it('1 - Returns updated car', async () => {
+      const carUpdated = await carModel.update('4edd40c86762e0fb12000003', createdCarMock);
+      expect(carUpdated).to.be.equal(createdCarMock);
+    });
+  });
+
+  describe('delete car', () => {
+    before(() => {
+      sinon.stub(mongoModel, 'findOneAndDelete')
+        .resolves(readCarsOne as (CarDocument & { _id: any }));
+    });
+
+    after(() => {
+      (mongoModel.findOneAndDelete as SinonStub).restore();
+    });
+
+    it('1 - Returns deleted car', async () => {
+      const carDeleted = await carModel.delete('4edd40c86762e0fb12000003');
+      expect(carDeleted).to.be.equal(readCarsOne);
     });
   });
 
