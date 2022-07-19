0273344db5dae4454f48f247984423bec862cef6
joaoOliveira <joaoavelino2008@hotmail.com>
2022-07-19 17:05:37 -0300

    feat(server): implements router for motorcycles

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,21 @@ import CustomRouter from './routes/Router';
 import App from './app';
 import CarController from './controllers/Cars.controller';
 import { Car } from './interfaces/CarInterface';
+import MotorcycleController from './controllers/Motorcycle.controller';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
 
 const server = new App();
 
 const carController = new CarController();
+const motorcycleController = new MotorcycleController();
 
 const carRouter = new CustomRouter<Car>();
+const motorcycleRouter = new CustomRouter<Motorcycle>();
+
 carRouter.addRoute(carController);
+motorcycleRouter.addRoute(motorcycleController);
 
 server.addRouter(carRouter.router);
+server.addRouter(motorcycleRouter.router);
 
 export default server;
