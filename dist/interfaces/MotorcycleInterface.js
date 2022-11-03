2ff6aa246889d4c47c5196b96e38159b54152dab
johnjoker13 <joaoavelino2008@hotmail.com>
2022-11-03 14:16:19 -0300

    build: remove dist

--- dist/interfaces/MotorcycleInterface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MotorcycleInterface = void 0;
-const zod_1 = require("zod");
-const VehicleInterface_1 = require("./VehicleInterface");
-exports.MotorcycleInterface = VehicleInterface_1.VehicleInterface.extend({
-    category: zod_1.z.enum(['Street', 'Custom', 'Trail']),
-    engineCapacity: zod_1.z.number().int().positive().lte(2500),
-});
